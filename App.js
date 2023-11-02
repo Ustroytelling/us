345e3d50e749121da2e29aa0efa3942bebec337d
fxrest704 <fxrest704@gmail.com>
2023-11-03 01:06:06 +0900

    Feat: 소설 메인 페이지 레이아웃 구성

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import NovelIndexScreen from "./src/screens/NovelIndexScreen";
 
 export default function App() {
-	return (
-		<View>
-		<Text>test1</Text>
-		<StatusBar style="auto" />
-		</View>
-	);
-};
+    return (
+        <SafeAreaView style={styles.container}>
+            <NovelIndexScreen />
+            <StatusBar style="auto" />
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+});
---
+++ src/data/NovelData.js
@@ -0,0 +1,94 @@
+import NovelCover from "../assets/novel_cover.png";
+
+export const novelIndexData = [
+    {
+        novel: {
+            id: "1",
+            image: NovelCover,
+            name: "주술회전",
+        },
+    },
+    {
+        novel: {
+            id: "2",
+            image: NovelCover,
+            name: "주술회전",
+        },
+    },
+    {
+        novel: {
+            id: "3",
+            image: NovelCover,
+            name: "주술회전",
+        },
+    },
+    {
+        novel: {
+            id: "4",
+            image: NovelCover,
+            name: "주술회전",
+        },
+    },
+    {
+        novel: {
+            id: "5",
+            image: NovelCover,
+            name: "주술회전",
+        },
+    },
+    {
+        novel: {
+            id: "6",
+            image: NovelCover,
+            name: "주술회전",
+        },
+    },
+    {
+        novel: {
+            id: "7",
+            image: NovelCover,
+            name: "주술회전",
+        },
+    },
+    {
+        novel: {
+            id: "8",
+            image: NovelCover,
+            name: "주술회전",
+        },
+    },
+    {
+        novel: {
+            id: "9",
+            image: NovelCover,
+            name: "주술회전",
+        },
+    },
+];
+
+export const hashtagData = [
+    {
+        id: "1",
+        tag: "드라마",
+    },
+    {
+        id: "2",
+        tag: "이세계",
+    },
+    {
+        id: "3",
+        tag: "웹",
+    },
+    {
+        id: "4",
+        tag: "액션물",
+    },
+    {
+        id: "5",
+        tag: "주술회전",
+    },
+    {
+        id: "6",
+        tag: "로맨스",
+    },
+];
