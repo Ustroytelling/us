86a37126c79c0a06eacf2a8c1f9e485c8dc54849
GayoungKim12 <cloud9b58@gmail.com>
2023-11-06 22:32:45 +0900

    Design: 디자인 관련 파일 추가

---
+++ src/assets/font.js
@@ -0,0 +1,17 @@
+export const fontSize = {
+  headline1: "16px",
+  headline2: "14px",
+  title1: "20pxx",
+  title2: "18px",
+  body1: "16px",
+  body2: "15px",
+  body3: "14px",
+  body4: "13px",
+  label1: "14px",
+};
+
+export const fontWeight = {
+  bold: 700,
+  medium: 500,
+  regular: 400,
+};
