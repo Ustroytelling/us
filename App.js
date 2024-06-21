01b0cb91795d9c1ef0d60c0a85f7f9c5978e1039
GayoungKim12 <cloud9b58@gmail.com>
2024-06-21 17:42:11 +0900

    라이브러리 업데이트로 인한 오류 수정

--- App.js
+++ App.js
@@ -4,14 +4,11 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import AppMainRoot from "./src/navigations/AppMainRoot";
-import NovelIndexScreen from "./src/screens/NovelMain/NovelIndexScreen";
-import SignInScreen from "./src/screens/SignIn/SignInScreen";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <NavigationContainer>
-        {/* <SignInScreen /> */}
         <AppMainRoot />
         <StatusBar style="rgba(255, 255, 255, 1)" />
       </NavigationContainer>
@@ -22,5 +19,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 36,
   },
 });
